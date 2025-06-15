0cacf1f59b2bf47114b38572daa2176a0a485f7a
JUSTICE123-ux <justiceogbonna349@gmail.com>
2025-06-15 17:46:19 +0100

    signup

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
+    console.log("Field required");
+    res.status(400).json({ message: "Field required" });
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    console.log("User registered successfully", {
+        firstName,
+        lastName,
+        email,
+        hashPassword
+    });
+    
+    res.send(`user ${firstName} registered successful`);
+
+  } catch (error) {
+    console.log("user failed to signup");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on localhost: ${PORT}`);
+});
